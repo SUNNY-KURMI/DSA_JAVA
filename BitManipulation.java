91a675f4bb6ac0f3f255c6b1af9182174daf8014
SUNNY-KURMI <kurmisunnyparmeshwar@gmail.com>
2026-06-09 13:54:29 +0530

    update3

--- BitManipulation.java
+++ BitManipulation.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class BitManipulation {
     public static void main (String[] args) {
     
@@ -7,16 +9,54 @@ public class BitManipulation {
     //clear
     //update
 
-    // set 
-    int n=5; // 0101
-    int pos = 3;
-    int left_shift = 1<<pos;
-    if((left_shift & n)== 0){
-        System.out.println("Bit was zero");
+    // Get Bit
+    // int n=5; // 0101
+    // int pos = 3;
+    // int left_shift = 1<<pos;
+    // if((left_shift & n)== 0){
+    //     System.out.println("Bit was zero");
+    // } else {
+    //     System.out.println("Bit was one");}
+
+    // set bit
+    // int n=5;// 0101
+    // int pos=2;
+    // int bitMask = 1<<pos;//0001 --> 0100
+
+    // int newNumber = bitMask | n; // 0100 | 0101 =0101
+    // System.out.println(newNumber);
+
+    //Clear Bit 
+    // int n=5;
+    // int pos = 2;
+    // int bitMask = 1<<pos;
+    // int notBitMask = ~(bitMask);
+    
+    // int newNumber = notBitMask & n;
+    // System.out.println(newNumber);
+
+    //Update Bit
+    Scanner sc = new Scanner(System.in);
+    int oper = sc.nextInt();
+    int n=5;
+    int pos=1;
+    
+    int bitMask=1<<pos;
+
+    // if oper = 1 then set bit else oper = 0 clear bit
+
+    if (oper == 1){
+        // Set Bit
+        int newNumber = bitMask | n;
+        System.out.println(newNumber);
     } else {
-        System.out.println("Bit was one");
+        // Clear Bit 
+        int notBitMask = ~(bitMask);
+        int newNumber = notBitMask & n ;
+        System.out.println(newNumber);
     }
 
+    
 
     }
     
