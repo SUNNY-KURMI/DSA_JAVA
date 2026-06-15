88f8feb4df15dcb6a9a6e13e1c3fd524ab04340b
SUNNY-KURMI <kurmisunnyparmeshwar@gmail.com>
2026-06-15 10:55:50 +0530

    update4

---
+++ Sorting.java
@@ -0,0 +1,67 @@
+public class Sorting{
+    public static void printArray(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+
+    }
+    public static void main(String[] args){
+        int arr[] = {7,8,3,1,2};
+
+        // //Bubble Short
+        // // time complexity = O(n^2)
+        // for (int i=0;i<arr.length-1;i++){ //n-1
+        //     for (int j=0;j<arr.length-i-1;j++){ //n-i-1 ---->i and 1 are constant so n*n=n^2
+        //         if(arr[j]>arr[j+1]){
+        //             //swap
+        //             int temp=arr[j];
+        //             arr[j]=arr[j+1];
+        //             arr[j+1]=temp;
+        //         }
+        //     }
+        // }
+
+        // printArray(arr);
+
+
+
+        // Selection sort 
+        // time complexity = O(n^2)
+        // for(int i=0;i<arr.length;i++){
+        //     int smallest = i;
+        //     for(int j=i+1;j<arr.length;j++){
+        //         if(arr[smallest]>arr[j]){
+        //             //swap
+        //             int temp = arr[smallest];
+        //             arr[smallest] = arr [j];
+        //             arr[j] = temp; 
+        //         }
+        //     }
+        // }
+        // printArray(arr);
+
+
+        // Insertion Sort
+        for(int i=1;i<arr.length;i++) {
+            int current = arr[i];
+            int j = i-1;
+            while(j >= 0 && current < arr[j]) {
+                arr[j+1] = arr[j];
+                j--;
+            }
+            // placement
+            arr[j+1]=current;
+        }
+
+        printArray(arr);
+
+        
+
+        
+
+
+
+
+    }
+}
